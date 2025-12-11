db703b3f7e6753094b8775b77f40feb195bb8fa1
HarshCodes14 <harshchoudhary14.sss@gmail.com>
2025-12-11 14:15:02 +0530

    Create Page.js

---
+++ Page.js
@@ -0,0 +1,16 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./component/navbar/page";
+import HomePage from "./component/dashboard/page";
+
+
+export default function Home() {
+  return (
+    <>
+      
+      <Navbar/>
+      <HomePage/>
+        
+    </>
+  );
+}
